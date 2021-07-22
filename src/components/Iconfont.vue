b2e506a673af6d5c79fa24899ec173fc9fbb640f
jeffjing <zgayjjf@qq.com>
2021-07-22 15:20:55 +0800

    feat: add iconfont.vue

--- src/components/Iconfont.vue
+++ src/components/Iconfont.vue
@@ -1,16 +1,22 @@
 <style lang="scss">
 .iconfont {
-
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
 
 <template>
-  <span class="iconfont">
-
-  </span>
+  <svg class="iconfont" aria-hidden="true" :style="style" v-on="$listeners">
+    <use :xlink:href="`#icon-${name}`" />
+  </svg>
 </template>
 
 <script>
+
+import { reactive } from '@nuxtjs/composition-api'
 
 export default {
   name: 'Iconfont',
@@ -19,6 +25,21 @@ export default {
     name: {
       type: String,
       default: '',
+    },
+    size: {
+      type: Number,
+      default: undefined,
+    }
+  },
+  setup (props) {
+    const style = reactive({})
+
+    if (props.size) {
+      style.height = style.width = `${props.size}px`
+    }
+
+    return {
+      style
     }
   }
 }
